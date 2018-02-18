53151ec2d62ad79adc27afd69ea6987d271840a1
hartmannn <armandenis@gmail.com>
2018-02-19 00:08:10 +0100

    console.log(userService.isUserKnown(senderID));

--- server/chatService.js
+++ server/chatService.js
@@ -22,6 +22,7 @@ function receivedMessage(event) {
   console.log("Received message for user %d and page %d at %d with message:",
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
+  console.log(userService.isUserKnown(senderID));
 
   var messageText = message.text;
   var messageAttachments = message.attachments;
